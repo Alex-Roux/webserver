0e5b152b1d7d8582a05ec27a7222dda30797e52a
Alex Roux <arouxandrey@gmail.com>
2020-12-21 18:49:59 +0100

    Add base structure
    Mostly form the Node.js crash course tutorial, from TheNetNinja

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fs = require('fs');
+/*
+// MongoDB
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+
+mongoose.connect(config.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => {
+    console.log("Connected to db.");
+    app.listen(3001);
+});
+
+
+// create the express app
+const app = express();
+
+
+// register view engine
+app.set('view engine', 'ejs');
+app.set('views', 'htdocs');
+
+
+// listen for requests
+app.listen(3000);
+
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+// Logger middleware
+
+
+
+// 404
+app.use((req, res) => {
+    res.render('404', { title: "404" });
+});
+
+*/
