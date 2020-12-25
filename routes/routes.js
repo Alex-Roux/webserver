8cf499e6beaf4d2bbad3df6bea315aec41dda05b
Alex Roux <arouxandrey@gmail.com>
2020-12-25 16:12:23 +0100

    Add a 2nd page

--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,10 @@ router.get("/", (req,res) => {
     res.render("index", { title: "Home"});
 });
 
+router.get("/page2", (req,res) => {
+    res.render("page2", { title: "Page 2"});
+});
+
+
 
 module.exports = router;
