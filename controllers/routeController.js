8b9e5e8bd07cb434c98db55505f85bd0600f2699
Alex Roux <arouxandrey@gmail.com>
2020-12-25 16:31:50 +0100

    eslint

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,14 +1,14 @@
 
 
-const main_index = (req, res) => {
+const mainIndex = (req, res) => {
     res.render("index", { title: "Home"});
 }
 
-const main_page2 = (req, res) => {
+const mainPage2 = (req, res) => {
     res.render("page2", { title: "Page 2"});
 }
 
 module.exports = {
-    main_index,
-    main_page2
+    mainIndex,
+    mainPage2
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ const router = new express.Router();
 const controller = require("../controllers/routeController");
 
 
-router.get("/", controller.main_index);
-router.get("/page2", controller.main_page2);
+router.get("/", controller.mainIndex);
+router.get("/page2", controller.mainPage2);
 
 
 module.exports = router;
