254448d0d44f8b45a4ec52a2736e82c58d0536a4
Alex Roux <arouxandrey@gmail.com>
2020-12-25 16:33:01 +0100

    eslint

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -2,13 +2,13 @@
 
 const mainIndex = (req, res) => {
     res.render("index", { title: "Home"});
-}
+};
 
 const mainPage2 = (req, res) => {
     res.render("page2", { title: "Page 2"});
-}
+};
 
 module.exports = {
     mainIndex,
     mainPage2
-}
+};
