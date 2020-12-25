ab61f6036d4ecd6db271b99be5a0ebb5187d03a3
Alex Roux <arouxandrey@gmail.com>
2020-12-25 19:11:18 +0100

    Add 2 more pages

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -8,7 +8,17 @@ const mainPage2 = (req, res) => {
     res.render("page2", { title: "Page 2"});
 };
 
+const mainPage3 = (req, res) => {
+    res.render("page3", { title: "Page 2"});
+};
+
+const mainPage4 = (req, res) => {
+    res.render("page4", { title: "Page 2"});
+};
+
 module.exports = {
     mainIndex,
-    mainPage2
+    mainPage2,
+    mainPage3,
+    mainPage4
 };
