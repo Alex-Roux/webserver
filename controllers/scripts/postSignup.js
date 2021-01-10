ccfc302c87f8f5fade3b487f8f560b4c6417530a
Alex Roux <arouxandrey@gmail.com>
2021-01-10 16:51:30 +0100

    var -> let

--- controllers/scripts/postSignup.js
+++ controllers/scripts/postSignup.js
@@ -2,14 +2,14 @@ const User = require("../../models/user");
 const utils = require("../../utils/utils.js");
 
 const postSignup = async function(req, res) {
-    const { email, password } = req.body;
+    let { email, password } = req.body;
     try {
-        const user = await User.create({ email, password });
-        const token = utils.createToken(user._id);
+        let user = await User.create({ email, password });
+        let token = utils.createToken(user._id);
         res.cookie("jwt", token, { httpOnly: true, maxAge: utils.maxAge * 1000 });
         res.status(201).json({ user: user._id });
     } catch(err) {
-        const errors = utils.databaseErrorHandler(err);
+        let errors = utils.databaseErrorHandler(err);
         res.status(400).json(errors);
     }
 }
