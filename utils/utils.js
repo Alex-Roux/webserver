40d1835afcf2074d082e8ee9ab393f50d467e608
Alex Roux <arouxandrey@gmail.com>
2021-01-10 16:51:06 +0100

    var -> let

--- utils/utils.js
+++ utils/utils.js
@@ -26,13 +26,13 @@ colors.setTheme({
 // This function is used instead of console.log();
 // as it also logs the string to latest.log
 function log(string, includeDate) {
-    var date;                                                                                                 // Define date
+    let date;                                                                                                 // Define date
     if(includeDate) {
         date = colors.grey("[" + new Date().toISOString().replace(/T/, " ").replace(/\..+/, "") + " GMT] ");  // Include the date 
     }                                                                                                         // -> [2021-01-01 12:01:23 GMT] String
 	string = date + string;                                                                                   // Add the date and the string together
 	console.log(string);                                                                                      // Log the string to the console
-    const regex = new RegExp(/(\x1B\x5B39m|\x1B\x5B90m|\x1B\x5B36m|\x1B\x5B31m|\x1B\x5B32m|\x1B\x5B33m)/gmu); // Define colors characters
+    let regex = new RegExp(/(\x1B\x5B39m|\x1B\x5B90m|\x1B\x5B36m|\x1B\x5B31m|\x1B\x5B32m|\x1B\x5B33m)/gmu); // Define colors characters
     string = string.replace(regex, "");                                                                       // Remove colors characters from the string
 	fs.appendFile("latest.log", string + "\r\n", function (err) {if (err) { throw err; }});                   // Append the string to latest.log
 }
