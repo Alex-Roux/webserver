6a4a1ab553c05c286dd3253eff810450d4948f63
Alex Roux <arouxandrey@gmail.com>
2021-01-10 17:22:42 +0100

    Handle login

--- controllers/scripts/postLogin.js
+++ controllers/scripts/postLogin.js
@@ -1,9 +1,23 @@
+<<<<<<< Updated upstream
 const utils = require("../../utils/utils.js");
+=======
+const utils = require("../../utils/utils")
+const User = require("../../models/user");
+>>>>>>> Stashed changes
 
 const postLogin = async function(req, res) {
+    let { email, password } = req.body;
 
-
-    res.render("/", { title: "Home"});
+    try {
+        let user = await User.login(email, password);
+        let token = utils.createToken(user._id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge: utils.maxAge * 1000 });
+        res.status(200).json({user: user._id});
+        utils.log("Logged in ".info + email, 1);
+    }
+    catch(err) {
+        res.status(400).json({});
+    }
 }
 
 module.exports = postLogin;
--- controllers/scripts/postSignup.js
+++ controllers/scripts/postSignup.js
@@ -1,5 +1,5 @@
+const utils = require("../../utils/utils");
 const User = require("../../models/user");
-const utils = require("../../utils/utils.js");
 
 const postSignup = async function(req, res) {
     const { email, password } = req.body;
@@ -9,7 +9,12 @@ const postSignup = async function(req, res) {
         res.cookie("jwt", token, { httpOnly: true, maxAge: utils.maxAge * 1000 });
         res.status(201).json({ user: user._id });
     } catch(err) {
+<<<<<<< Updated upstream
         const errors = utils.databaseErrorHandler(err);
+=======
+        log("Failed signup.".warn, 1);
+        let errors = utils.databaseErrorHandler(err);
+>>>>>>> Stashed changes
         res.status(400).json(errors);
     }
 }
